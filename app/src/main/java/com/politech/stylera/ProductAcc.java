22e18d6a9798bae6c6a559bc66c8d92c7cf82c5d
betodo03 <betodo03@gamil.com>
2019-06-19 09:21:15 +0900

    3

--- app/src/main/java/com/politech/stylera/ProductAcc.java
+++ app/src/main/java/com/politech/stylera/ProductAcc.java
@@ -1,4 +1,15 @@
 package com.politech.stylera;
 
-public class ProductAcc {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProductAcc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.product);
+
+    }
 }
--- app/src/main/java/com/politech/stylera/SearchAcc.java
+++ app/src/main/java/com/politech/stylera/SearchAcc.java
@@ -3,16 +3,28 @@ package com.politech.stylera;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SearchAcc extends AppCompatActivity {
     ImageButton cateBtn, searchBtn, bookmarkBtn, boardBtn, setUpBtn;
+    Button search_searchBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search);
+
+        search_searchBtn = (Button) findViewById(R.id.search_searchBtn);
+        search_searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ProductAcc.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         cateBtn = (ImageButton) findViewById(R.id.cateBtn);
         cateBtn.setOnClickListener(new View.OnClickListener() {
